5c1a4233c1a5e0c0779c35c34ad67952fadc6d61
Orry A. Jarvis <oajarvis@sep.com>
2022-04-28 10:57:27 -0400

    renamed home-> about and gave blog priority

--- projects/blog/src/app/about/about-routing.module.ts
+++ projects/blog/src/app/about/about-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class AboutRoutingModule { }
--- projects/blog/src/app/app-routing.module.ts
+++ projects/blog/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: '404', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/blog', pathMatch: 'full' },
   { path: '**', redirectTo: '/404' }
 ];
 
